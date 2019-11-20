9b004a51deef133c1ae7ad79118656504cb00443
elirud <eli.rud@hotmail.com>
2019-11-20 14:20:10 +0100

    Initial commit och code and test input for B1

---
+++ B1_Luhns.py
@@ -0,0 +1,47 @@
+with open("test.txt", "r") as f:
+    data = f.read().splitlines()
+
+answer = []
+
+for number in data:
+    print(number)
+    double_x = None
+    check_sum = 0
+
+    for character in number[-2::-2]:
+        if character == 'X':
+            double_x = True
+            continue
+        if int(character) * 2 < 10:
+            check_sum += int(character) * 2
+        else:
+            check_sum += (int(character) * 2) - 9
+
+    for character in number[-3::-2]:
+        if character == 'X':
+            double_x = False
+            continue
+        check_sum += int(character)
+
+    if double_x is None:  # X is the check digit at the end
+        for i in range(10):
+            if (check_sum + i) % 10 == 0:
+                answer.append(str(i))
+                break
+        continue
+
+    check_digit = int(number[len(number) - 1])
+
+    for i in range(10):
+        j = i
+        if double_x:
+            j = i * 2
+            if j > 10:
+                j = j - 9
+        if (check_sum + j + check_digit) % 10 == 0:
+            print(i)
+            answer.append(str(i))
+            break
+
+print(''.join(answer))
+print(len(answer))
