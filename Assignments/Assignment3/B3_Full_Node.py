a8f1287504ef19b6b3817b2f2797e80154374232
Sofi <sofi.flinck@gmail.com>
2019-11-24 22:18:14 +0100

    non working solutions

---
+++ Assignments/Assignment3/B3_Full_Node.py
@@ -0,0 +1,55 @@
+import hashlib
+import math
+
+
+def calc_parents(children):
+    parents = []
+
+    for leaf in children[::2]:
+        parents.append(sha1_hash_hexadecimal_value(leaf + children[children.index(leaf) + 1]))
+
+    return parents
+
+
+def sha1_hash_hexadecimal_value(hstring):
+    return hashlib.sha1(bytearray.fromhex(hstring)).hexdigest()
+
+
+with open('testB3_2.txt', 'r') as f:
+    data = f.read().splitlines()
+
+
+n_leaf = int(data[0])
+n_sibling_depth = int(data[1])
+depth = int(math.ceil(math.log2(len(data[3:]))))
+leaves = data[3:]
+merkle_path = []
+merkle_node_at_depth = None
+
+
+for i in range(depth):
+    if len(leaves) % 2 != 0:
+        leaves.append(leaves[len(leaves) - 1])
+    if n_leaf % 2 == 0:
+        merkle_path.append('R' + leaves[n_leaf + 1])
+    else:
+        merkle_path.append('L' + leaves[n_leaf - 1])
+    if (depth - i) == n_sibling_depth:
+        merkle_node_at_depth = merkle_path[len(merkle_path) - 1]
+    leaves = calc_parents(leaves)
+    n_leaf = int(math.ceil(n_leaf / 2))
+
+print("-----Merkle Path-----")
+for sibling in merkle_path:
+    print(sibling)
+print("---------------------")
+
+
+print("-----Merkle Node-----")
+print(merkle_node_at_depth)
+print("---------------------")
+
+
+print("-----Merkle Root-----")
+print(leaves)
+print("---------------------")
