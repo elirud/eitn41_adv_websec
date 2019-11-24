2d98c83cac7ef35e3bf7afa453e433e2dbc6b38c
Sofi <sofi.flinck@gmail.com>
2019-11-24 20:21:58 +0100

    m

--- Assignments/Assignment3/B3_SPV_Node.py
+++ Assignments/Assignment3/B3_SPV_Node.py
@@ -1,12 +1,5 @@
 import hashlib
 
-def integer_to_hexadecimal(istring):
-    return str(hex(istring))
-
-
-def hexadecimal_to_integer(hstring):
-    return str(int(hstring, 16))
-
 
 def sha1_hash_hexadecimal_value(hstring):
     return hashlib.sha1(bytearray.fromhex(hstring)).hexdigest()
@@ -22,4 +15,3 @@ for node in data[1:]:
     built_hash = sha1_hash_hexadecimal_value(node[1:] + built_hash) if node[0] == 'L' else sha1_hash_hexadecimal_value(built_hash + node[1:])
 
 print(built_hash)
-
