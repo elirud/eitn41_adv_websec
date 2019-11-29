af3badedaad87393ea530ef5a4d8e2696ae23fee
Sofi <sofi.flinck@gmail.com>
2019-11-29 13:21:19 +0100

    b2

--- Assignments HA2/Assignment2/B2_Disclosure_Attack.py
+++ Assignments HA2/Assignment2/B2_Disclosure_Attack.py
@@ -4,8 +4,8 @@ from pcapfile import savefile
 abu_ipaddress = input("Please enter Abu Nazir's IP-address: ")
 mix_ipaddress = input("Please enter Mix's IP-address: ")
 n_partners = input("Please enter number of partners of Abu Nazir: ")
-traffic = open('test.pcap', 'rb')
-capfile = savefile.load_savefile(traffic, layers=2, verbose=True)
+with open('test.pcap', 'rb') as traffic:
+    capfile = savefile.load_savefile(traffic, layers=2, verbose=True)
 
 
 # print the packets
@@ -14,8 +14,8 @@ for pkt in capfile.packets:
     timestamp = pkt.timestamp
     # all data is ASCII encoded (byte arrays). If we want to compare with strings
     # we need to decode the byte arrays into UTF8 coded strings
-    eth_src = pkt.packet.src.decode('UTF8')
-    eth_dst = pkt.packet.dst.decode('UTF8')
+    eth_src = pkt.packet.src.decode('UTF16')
+    eth_dst = pkt.packet.dst.decode('UTF16')
     ip_src = pkt.packet.payload.src.decode('UTF8')
     ip_dst = pkt.packet.payload.dst.decode('UTF8')
     print ('{}\t\t{}\t{}\t{}\t{}'.format(timestamp, eth_src, eth_dst, ip_src, ip_dst))
