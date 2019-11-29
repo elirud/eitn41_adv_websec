bd7d3fb1ab0804767d78fe8dd5ae2c9936373e60
Elias <eli.rud@hotmail.com>
2019-11-29 16:00:14 +0100

    ...

--- Assignments HA2/Assignment2/B2_Disclosure_Attack.py
+++ Assignments HA2/Assignment2/B2_Disclosure_Attack.py
@@ -1,21 +1,21 @@
 from pcapfile import savefile
 
 
-abu_ipaddress = input("Please enter Abu Nazir's IP-address: ")
-mix_ipaddress = input("Please enter Mix's IP-address: ")
-n_partners = input("Please enter number of partners of Abu Nazir: ")
+abu_ipaddress = "159.237.13.37"  # input("Please enter Abu Nazir's IP-address: ")
+mix_ipaddress = "94.147.150.188"  # input("Please enter Mix's IP-address: ")
+n_partners = 2  # input("Please enter number of partners of Abu Nazir: ")
 with open('test.pcap', 'rb') as traffic:
     capfile = savefile.load_savefile(traffic, layers=2, verbose=True)
 
 
 # print the packets
-print ('timestamp\teth src\t\t\teth dst\t\t\tIP src\t\tIP dst')
+print ('timestamp\t\teth src\t\t\t\teth dst\t\t\t\tIP src\t\t\tIP dst')
 for pkt in capfile.packets:
     timestamp = pkt.timestamp
     # all data is ASCII encoded (byte arrays). If we want to compare with strings
     # we need to decode the byte arrays into UTF8 coded strings
-    eth_src = pkt.packet.src.decode('UTF16')
-    eth_dst = pkt.packet.dst.decode('UTF16')
+    eth_src = pkt.packet.src.decode('UTF8')
+    eth_dst = pkt.packet.dst.decode('UTF8')
     ip_src = pkt.packet.payload.src.decode('UTF8')
     ip_dst = pkt.packet.payload.dst.decode('UTF8')
     print ('{}\t\t{}\t{}\t{}\t{}'.format(timestamp, eth_src, eth_dst, ip_src, ip_dst))
