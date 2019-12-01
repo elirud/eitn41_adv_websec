72a155c7449c8847e8e950d71c68895722b93a34
Sofi <sofi.flinck@gmail.com>
2019-12-01 21:14:57 +0100

    b3

--- Assignments HA2/Assignment2/B2_Disclosure_Attack.py
+++ Assignments HA2/Assignment2/B2_Disclosure_Attack.py
@@ -1,10 +1,18 @@
 from pcapfile import savefile
+from ipaddress import ip_address
+
+def intersection(lst1, lst2):
+    lst3 = [list(filter(lambda x: x in lst1, sublist)) for sublist in lst2]
+    return lst3
 
 
-abu_ipaddress = "159.237.13.37"  # input("Please enter Abu Nazir's IP-address: ")
-mix_ipaddress = "94.147.150.188"  # input("Please enter Mix's IP-address: ")
-n_partners = 2  # input("Please enter number of partners of Abu Nazir: ")
-with open('test.pcap', 'rb') as traffic:
+
+nazir_ipaddress = "161.53.13.37"  # input("Please enter Abu Nazir's IP-address: ")
+mix_ipaddress = "11.192.206.171"  # input("Please enter Mix's IP-address: ")
+n_partners = 12  # input("Please enter number of partners of Abu Nazir: ")
+
+
+with open('test2.pcap', 'rb') as traffic:
     capfile = savefile.load_savefile(traffic, layers=2, verbose=True)
 
 
@@ -20,25 +28,74 @@ for pkt in capfile.packets:
     eth_dst = pkt.packet.dst.decode('UTF8')
     ip_src = pkt.packet.payload.src.decode('UTF8')
     ip_dst = pkt.packet.payload.dst.decode('UTF8')
-    if ip_src == mix_ipaddress and not mix_hasSent:
+    timestamp = pkt.timestamp
+
+    prev_mix_hasSent = mix_hasSent
+    if ip_src != mix_ipaddress:
+        mix_hasSent = False
+        temp = []
+    if ip_src == mix_ipaddress:
         mix_hasSent = True
-    if ip_src == abu_ipaddress:
+        temp.append(ip_dst)
+    if ip_src == nazir_ipaddress:
         nazir_hasSent = True
-    if ip_src != mix_ipaddress and mix_hasSent:
-        if nazir_hasSent:
+    if not prev_mix_hasSent and mix_hasSent:
+        if nazir_hasSent and mix_hasSent:
             nazir_batches.append(temp)
             nazir_hasSent = False
-        temp.clear()
-        mix_hasSent = False
-    timestamp = pkt.timestamp
+
+
     # all data is ASCII encoded (byte arrays). If we want to compare with strings
     # we need to decode the byte arrays into UTF8 coded strings
-    temp_eth_src = eth_src
-    temp_eth_dst = eth_dst
-    temp_ip_src = ip_src
-    temp_ip_dst = ip_dst
-    if mix_hasSent:
-        temp.append(ip_dst)
 
+    # print ('{}\t\t{}\t{}\t{}\t{}'.format(timestamp, eth_src, eth_dst, ip_src, ip_dst))
+print(nazir_batches)
+nazir_set_batches = []
+nazir_unique_batches = []
 
-    print ('{}\t\t{}\t{}\t{}\t{}'.format(timestamp, eth_src, eth_dst, ip_src, ip_dst))
+for batch in nazir_batches:
+    nazir_set_batches.append(set(batch))
+
+for i in range(0, len(nazir_set_batches) - 1):
+    batchi = nazir_set_batches[i]
+    for j in range(i + 1, len(nazir_set_batches)):
+        batchj = nazir_set_batches[j]
+        if batchi.isdisjoint(batchj):
+            unique_batchi = True
+            unique_batchj = True
+            for k in range(len(nazir_unique_batches)):
+                batch = nazir_unique_batches[k]
+                if not batchi.isdisjoint(batch):
+                    unique_batchi = False
+                if not batchj.isdisjoint(batch):
+                    unique_batchj = False
+            if unique_batchi:
+                nazir_unique_batches.append(batchi)
+            if unique_batchj:
+                nazir_unique_batches.append(batchj)
+        if len(nazir_unique_batches) == n_partners:
+            break
+    if len(nazir_unique_batches) == n_partners:
+        break
+
+oneMatch = True
+recipients = []
+
+for batchu in nazir_unique_batches:
+    for batchi in nazir_set_batches:
+        if len(batchu) == 1:
+            recipients.append(list(batchu)[0])
+            break
+        if not len(batchu.intersection(batchi)) == 0:
+            for batchj in nazir_unique_batches:
+                if batchu == batchj:
+                    continue
+                if not len(batchi.intersection(batchj)) == 0:
+                    oneMatch = False
+                    break
+            if oneMatch:
+                batchu.intersection_update(batchi)
+            oneMatch = True
+
+print(recipients)
+print(sum(list(map(lambda x: int(ip_address(x)), recipients))))
