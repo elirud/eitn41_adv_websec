142abde9e635e921631c890f57b7c747c6b9294f
Elias Rudberg <eli.rud@hotmail.com>
2019-12-06 20:34:15 +0100

    Adds test algorithm

--- Assignments HA3/Assignment2/B2_Treshold_Scheme.py
+++ Assignments HA3/Assignment2/B2_Treshold_Scheme.py
@@ -5,11 +5,18 @@ poly_shares = int(input("Please enter polynomial shares from collaborating parti
 degree = k_val - 1
 deactivation_code = 0
 
+shared_polys = {1: 468, 2: 2784, 4: 20822, 5: 70960, 7: 256422}
 
+collaborators = [1, 2, 4, 5, 7]
+x = 0
+quotient = 1
+for i in collaborators:
+    for j in collaborators:
+        if j != i:
+            quotient *= j / (j - i)
+    x += shared_polys.get(i) * quotient
+    print(x)
+    quotient = 1
 
-
-
-
-
-
-print(deactivation_code)
+print(x)
+# print(deactivation_code)
