bd06a59c75fab3bc4bab7c353ab46f54a58d6f82
Sofi <sofi.flinck@gmail.com>
2019-12-06 19:17:08 +0100

    fix

--- Assignments HA3/Assignment2/B2_Treshold_Scheme.py
+++ Assignments HA3/Assignment2/B2_Treshold_Scheme.py
@@ -2,6 +2,7 @@ k_val = int(input("Please enter k-value: "))
 n_val = int(input("Please enter n-value: "))
 private_poly = int(input("Please enter private polynomial: "))
 poly_shares = int(input("Please enter polynomial shares from collaborating participants: "))
+degree = k_val - 1
 deactivation_code = 0
 
 
