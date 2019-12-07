8f12ff8b7acc66d650e5f8746ddbe9fcd02aecca
Elias <eli.rud@hotmail.com>
2019-12-07 21:03:39 +0100

    Fixes small mistakes

--- Assignments HA3/Assignment1/B1_Commitment_Scheme.py
+++ Assignments HA3/Assignment1/B1_Commitment_Scheme.py
@@ -1,10 +1,10 @@
 from hashlib import sha1
 
 
-zero_values = ["0" + str(bin(x)).zfill(16) for x in range(2 ** 16 - 1)]
-one_values = ["1" + str(bin(x)).zfill(16) for x in range(2 ** 16 - 1)]
-zero_hashes = [bin(int(sha1(x.encode()).hexdigest(), 16)).lstrip("0b")[:160] for x in zero_values]
-one_hashes = [bin(int(sha1(x.encode()).hexdigest(), 16)).lstrip("0b")[:160] for x in one_values]
+zero_values = ["0" + bin(x).lstrip("0b").zfill(16) for x in range(2 ** 16)]
+one_values = ["1" + bin(x).lstrip("0b").zfill(16) for x in range(2 ** 16)]
+zero_hashes = [bin(int(sha1(x.encode()).hexdigest(), 16)).lstrip("0b")[:16] for x in zero_values]
+one_hashes = [bin(int(sha1(x.encode()).hexdigest(), 16)).lstrip("0b")[:16] for x in one_values]
 
 
 print(zero_hashes[0])
