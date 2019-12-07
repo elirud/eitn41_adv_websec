faccc5ef4bcf3085812e4b41f5d8dba50d54ba84
Elias <eli.rud@hotmail.com>
2019-12-07 14:59:16 +0100

    Adds creation of lists with values and corresponding hashes

--- Assignments HA3/Assignment1/B1_Commitment_Scheme.py
+++ Assignments HA3/Assignment1/B1_Commitment_Scheme.py
@@ -1,10 +1,10 @@
-import hashlib
-hash = hashlib.sha1()
+from hashlib import sha1
 
-hashlib.sha1(b"Nobody inspects the spammish repetition").hexdigest()
 
-k_combinations = (2**16 - 1) * 2
-X0_size = 16
-X1_size = 24
-X2_size = 32
-X4_size = 40
+zero_values = ["0" + str(bin(x)).zfill(16) for x in range(2 ** 16 - 1)]
+one_values = ["1" + str(bin(x)).zfill(16) for x in range(2 ** 16 - 1)]
+zero_hashes = [bin(int(sha1(x.encode()).hexdigest(), 16)).lstrip("0b")[:160] for x in zero_values]
+one_hashes = [bin(int(sha1(x.encode()).hexdigest(), 16)).lstrip("0b")[:160] for x in one_values]
+
+
+print(zero_hashes[0])
