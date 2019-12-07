7b96ef23fcad68234ecf6617aee260988295d003
Elias <eli.rud@hotmail.com>
2019-12-07 21:05:08 +0100

    Adds working B1_Commitment_Scheme

--- Assignments HA3/Assignment1/B1_Commitment_Scheme.py
+++ Assignments HA3/Assignment1/B1_Commitment_Scheme.py
@@ -6,5 +6,11 @@ one_values = ["1" + bin(x).lstrip("0b").zfill(16) for x in range(2 ** 16)]
 zero_hashes = [bin(int(sha1(x.encode()).hexdigest(), 16)).lstrip("0b")[:16] for x in zero_values]
 one_hashes = [bin(int(sha1(x.encode()).hexdigest(), 16)).lstrip("0b")[:16] for x in one_values]
 
+amount_of_matches = []
+for y in one_hashes:
+    matches = [x for x in zero_hashes if x == y]
+    if len(matches) != 0:
+        amount_of_matches.append(len(matches))
 
-print(zero_hashes[0])
+print(len(amount_of_matches))
+print(amount_of_matches[:10])
