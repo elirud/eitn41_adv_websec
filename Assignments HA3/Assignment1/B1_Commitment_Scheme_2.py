3f3970a08373b41f319852b48c3884f022365da7
Elias <eli.rud@hotmail.com>
2019-12-08 16:18:25 +0100

    ...

--- Assignments HA3/Assignment1/B1_Commitment_Scheme_2.py
+++ Assignments HA3/Assignment1/B1_Commitment_Scheme_2.py
@@ -7,16 +7,14 @@ hash_lengths = [1, 2, 4, 8, 16, 32, 35, 64]
 
 probability_for_hash_length = {}
 
-amount_of_matches = []
 for hash_length in hash_lengths:
     zero_hashes = [bin(int(sha1(x.encode()).hexdigest(), 16)).lstrip("0b")[:hash_length] for x in zero_values]
     one_hashes = [bin(int(sha1(x.encode()).hexdigest(), 16)).lstrip("0b")[:hash_length] for x in one_values]
+    amount_of_matches = []
     for y in one_hashes:
         matches = [x for x in zero_hashes if x == y]
         if len(matches) != 0:
             amount_of_matches.append(len(matches))
     probability_for_hash_length.update({hash_length: ((2 ** 16 - len(amount_of_matches)) / (2 ** 16))})
 
-print(len(amount_of_matches))
-print("Probability of x being unique: ", ((2 ** 16 - len(amount_of_matches)) / (2 ** 16)))
-
+print(probability_for_hash_length)
