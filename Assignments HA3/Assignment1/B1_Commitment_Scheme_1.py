b75e798338f25d2a3f48162224c8937c37d7e626
Sofi <sofi.flinck@gmail.com>
2019-12-08 15:59:27 +0100

    Adds plotting of graph

--- Assignments HA3/Assignment1/B1_Commitment_Scheme_1.py
+++ Assignments HA3/Assignment1/B1_Commitment_Scheme_1.py
@@ -1,11 +1,13 @@
 from hashlib import sha1
-import matplotlib
+import matplotlib.pyplot as plt
+import collections
 
 
 zero_values = ["0" + bin(x).lstrip("0b").zfill(16) for x in range(2 ** 16)]
 one_values = ["1" + bin(x).lstrip("0b").zfill(16) for x in range(2 ** 16)]
 hash_lengths = [1, 2, 4, 8, 16, 32, 35, 64]
 match_found_for_length = {}
+
 
 for hash_length in hash_lengths:
     zero_hashes = [bin(int(sha1(x.encode()).hexdigest(), 16)).lstrip("0b")[:hash_length] for x in zero_values]
@@ -17,4 +19,12 @@ for hash_length in hash_lengths:
             break
     match_found_for_length.update({hash_length: match_found})
 
+
+fig = plt.figure()
+plt.plot(*zip(*sorted(match_found_for_length.items())), marker='o')
+fig.suptitle('Probability of breaking the binding property', fontsize=20)
+plt.xlabel('X-value', fontsize=18)
+plt.ylabel('Probability', fontsize=16)
+fig.savefig('graph.png')
+plt.show()
 print(match_found_for_length)
