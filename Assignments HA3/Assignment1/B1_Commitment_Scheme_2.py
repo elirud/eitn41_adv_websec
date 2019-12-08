131bbbfca353f3abec234cc37369b380ec553994
Elias <eli.rud@hotmail.com>
2019-12-08 16:16:16 +0100

    ...

--- Assignments HA3/Assignment1/B1_Commitment_Scheme_2.py
+++ Assignments HA3/Assignment1/B1_Commitment_Scheme_2.py
@@ -3,14 +3,14 @@ from hashlib import sha1
 
 zero_values = ["0" + bin(x).lstrip("0b").zfill(16) for x in range(2 ** 16)]
 one_values = ["1" + bin(x).lstrip("0b").zfill(16) for x in range(2 ** 16)]
-zero_hashes = [bin(int(sha1(x.encode()).hexdigest(), 16)).lstrip("0b")[:16] for x in zero_values]
-one_hashes = [bin(int(sha1(x.encode()).hexdigest(), 16)).lstrip("0b")[:16] for x in one_values]
 hash_lengths = [1, 2, 4, 8, 16, 32, 35, 64]
 
 probability_for_hash_length = {}
 
 amount_of_matches = []
 for hash_length in hash_lengths:
+    zero_hashes = [bin(int(sha1(x.encode()).hexdigest(), 16)).lstrip("0b")[:hash_length] for x in zero_values]
+    one_hashes = [bin(int(sha1(x.encode()).hexdigest(), 16)).lstrip("0b")[:hash_length] for x in one_values]
     for y in one_hashes:
         matches = [x for x in zero_hashes if x == y]
         if len(matches) != 0:
