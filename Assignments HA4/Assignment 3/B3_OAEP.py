78eed63e98e9a04e188b14cf1562fe75cc9c50d6
Elias <eli.rud@hotmail.com>
2019-12-12 16:30:01 +0100

    ...

--- Assignments HA4/Assignment 3/B3_OAEP.py
+++ Assignments HA4/Assignment 3/B3_OAEP.py
@@ -1,7 +1,18 @@
 from hashlib import sha1
+from math import ceil
+
 
 
 def i20sp(x, xlen):
-    return str(int(x, 256)).zfill(xlen)
 
 
+
+def mgf1(seed, mask_len):
+    t = ''
+    for i in range(ceil(mask_len / 20)):
+        c = i20sp(i, 4)
+        t += sha1((seed + c).encode()).hexdigest()
+
+    return t[:2*mask_len]
+
+print(mgf1("0123456789abcdef", 30))
