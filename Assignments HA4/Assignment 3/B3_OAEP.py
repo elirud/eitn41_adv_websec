73053017616e7355ef9175fd0e9e1c3054de464c
Elias Rudberg <eli.rud@hotmail.com>
2019-12-13 15:41:13 +0100

    Adds working mgf1 and i2osp functions

--- Assignments HA4/Assignment 3/B3_OAEP.py
+++ Assignments HA4/Assignment 3/B3_OAEP.py
@@ -3,24 +3,19 @@ from math import ceil
 import sys
 
 
-def i20sp(x, xlen):
+def i2osp(x, xlen):
     if x >= 256**xlen:
         print("integer too large")
         sys.exit()
-    temp_x = str(x).zfill(xlen)
-    x = ''
-    temp = 0
-    for i in range(1, xlen + 1):
-        x += temp_x[xlen - i] * 256**(xlen - i)
 
-    return x
+    return hex(x).lstrip('0x').zfill(2 * xlen)
 
 
 def mgf1(seed, mask_len):
     t = ''
     for i in range(ceil(mask_len / 20)):
-        c = i20sp(i, 4)
-        t += sha1((seed + c).encode()).hexdigest()
+        c = i2osp(i, 4)
+        t += sha1(bytearray.fromhex(seed + c)).hexdigest()
 
     return t[:2*mask_len]
 
