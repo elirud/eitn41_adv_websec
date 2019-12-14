defd790ee0acaa6fa307a4a264e71e8f7ccd89d4
Elias Rudberg <eli.rud@hotmail.com>
2019-12-14 14:55:54 +0100

    Starts working on OAEP_encode

--- Assignments HA4/Assignment 3/B3_OAEP.py
+++ Assignments HA4/Assignment 3/B3_OAEP.py
@@ -19,4 +19,14 @@ def mgf1(seed, mask_len):
 
     return t[:2*mask_len]
 
+
+def OAEP_encode(message, seed):
+    h_len = int(len(message)/2)
+    l_hash = bytearray.fromhex(sha1('').hexdigest())
+    ps = bytearray(128 - h_len - 40 - 2)
+    db = l_hash + ps + b'\x01' + bytearray.fromhex(message)
+    db_mask = bytearray.fromhex(mgf1(seed, 128 - h_len - 1))
+    masked_db =
+    masked_seed = bin(int(seed, 16)) ^
+
 print(mgf1("0123456789abcdef", 30))
