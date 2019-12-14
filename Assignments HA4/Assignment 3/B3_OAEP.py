286bcd43388ac4c7cb7e2bcc5e93b8315ac90d9c
Sofi <sofi.flinck@gmail.com>
2019-12-14 15:08:59 +0100

    bxor

--- Assignments HA4/Assignment 3/B3_OAEP.py
+++ Assignments HA4/Assignment 3/B3_OAEP.py
@@ -19,6 +19,11 @@ def mgf1(seed, mask_len):
 
     return t[:2*mask_len]
 
+def bxor(b1, b2): # use xor for bytes
+    result = bytearray()
+    for b1, b2 in zip(b1, b2):
+        result.append(b1 ^ b2)
+    return result
 
 def OAEP_encode(message, seed):
     h_len = int(len(message)/2)
@@ -26,7 +31,9 @@ def OAEP_encode(message, seed):
     ps = bytearray(128 - h_len - 40 - 2)
     db = l_hash + ps + b'\x01' + bytearray.fromhex(message)
     db_mask = bytearray.fromhex(mgf1(seed, 128 - h_len - 1))
-    masked_db =
-    masked_seed = bin(int(seed, 16)) ^
+    masked_db = bxor(db, db_mask)
+    seedMask = mgf1(masked_db, h_len)
+    masked_seed = bxor(bytearray.fromhex(seed) , seedMask)
+    EM = "0x00" + str(masked_seed).lstrip('0x') + str(masked_db).lstrip('0x')
 
 print(mgf1("0123456789abcdef", 30))
