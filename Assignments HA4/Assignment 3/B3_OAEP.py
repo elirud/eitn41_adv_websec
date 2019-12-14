e82f1bb7796762c645b199286fa83e0c9471a4fe
Elias Rudberg <eli.rud@hotmail.com>
2019-12-14 15:22:42 +0100

    Adds ALMOST working OAEP_encode

--- Assignments HA4/Assignment 3/B3_OAEP.py
+++ Assignments HA4/Assignment 3/B3_OAEP.py
@@ -19,21 +19,18 @@ def mgf1(seed, mask_len):
 
     return t[:2*mask_len]
 
-def bxor(b1, b2):
-    result = bytearray(b1)
-    for i, b in enumerate(b2):
-        result[i] ^= b
-    return bytes(result)
 
 def OAEP_encode(message, seed):
     h_len = int(len(message)/2)
-    l_hash = bytearray.fromhex(sha1('').hexdigest())
+    l_hash = bytearray.fromhex(sha1(b'').hexdigest())
     ps = bytearray(128 - h_len - 40 - 2)
     db = l_hash + ps + b'\x01' + bytearray.fromhex(message)
     db_mask = bytearray.fromhex(mgf1(seed, 128 - h_len - 1))
-    masked_db = bxor(db, db_mask)
-    seedMask = mgf1(masked_db, h_len)
-    masked_seed = bxor(bytearray.fromhex(seed) , seedMask)
-    EM = "0x00" + str(masked_seed).lstrip('0x') + str(masked_db).lstrip('0x')
+    masked_db = bytes(a ^ b for a, b in zip(db, db_mask))
+    seed_mask = bytearray.fromhex(mgf1(masked_db, h_len))
+    masked_seed = bytes(a ^ b for a, b in zip(seed, seed_mask))
+    em = b"\x00" + masked_seed + masked_db
+    return em
 
 print(mgf1("0123456789abcdef", 30))
+print(OAEP_encode("fd5507e917ecbe833878", "1e652ec152d0bfcd65190ffc604c0933d0423381"))
